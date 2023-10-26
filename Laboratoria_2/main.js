0ed3e4e087fcca44ee25c3804d8957e84be2b3d4
Sebastian2k21 <seba0071@interia.pl>
2023-10-26 20:29:37 +0200

    Update task 2

--- Laboratoria_2/main.js
+++ Laboratoria_2/main.js
@@ -1,39 +1,51 @@
-let slideIndex = 1;
+let currentSlide = 0;
+const slides = document.querySelectorAll(".slide");
+const sliderWrapper = document.querySelector(".slider-wrapper");
+const paginationContainer = document.querySelector(".pagination");
 
-let moveRightInterval = setInterval(function() {
-    plusSlides(1);
-}, 5000);
-
-function plusSlides(n) {
-    showSlides(slideIndex += n);
+function updateSlide() {
+    sliderWrapper.style.transform = `translateX(-${100 * currentSlide}%)`;
+    updateDots();
 }
 
-function currentSlide(n) {
-    showSlides(slideIndex = n);
+function updateDots() {
+    const dots = document.querySelectorAll(".pagination span");
+    dots.forEach((dot, index) => {
+        dot.style.backgroundColor = index === currentSlide ? "#333" : "#ccc";
+    });
 }
 
-function showSlides(n) {
-    clearInterval(moveRightInterval);
-    
-    let slidesContainer = document.getElementById("slider").querySelector(".slides");
-    let slides = slidesContainer.querySelectorAll("img");
-    let dots = document.getElementsByClassName("dot");
-    
-    if (n > slides.length) { slideIndex = 1; } 
-    if (n < 1) { slideIndex = slides.length; }
-    
-    for (let i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
+slides.forEach((_, index) => {
+    const dot = document.createElement("span");
+    dot.onclick = () => {
+        currentSlide = index;
+        updateSlide();
+    };
+    paginationContainer.appendChild(dot);
+});
+
+function nextSlide() {
+    currentSlide = (currentSlide + 1) % slides.length;
+    updateSlide();
+}
+
+function prevSlide() {
+    currentSlide = (currentSlide - 1 + slides.length) % slides.length;
+    updateSlide();
+}
+
+let interval = setInterval(nextSlide, 3000);
+
+document.getElementById("nextSlide").onclick = nextSlide;
+document.getElementById("prevSlide").onclick = prevSlide;
+
+document.getElementById("pauseResume").onclick = function() {
+    if (interval) {
+        clearInterval(interval);
+        interval = null;
+        this.textContent = "Wznów";
+    } else {
+        interval = setInterval(nextSlide, 3000);
+        this.textContent = "Pauza";
     }
-    
-    for (let i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
-    }
-    
-    slides[slideIndex - 1].style.display = "block";
-    dots[slideIndex - 1].className += " active";
-    
-    moveRightInterval = setInterval(function() {
-        plusSlides(1);
-    }, 5000);
-}
+};
